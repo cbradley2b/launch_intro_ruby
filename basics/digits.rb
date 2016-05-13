2af0ae312f8f2f6b28896661404145bc055362d0
Clayton Bradley <cbradley2b@gmail.com>
2016-05-12 21:57:20 -0400

    solve using modulo and division

---
+++ basics/digits.rb
@@ -0,0 +1,15 @@
+#Use the modulo operator, division, or a combination of both to take a 4 digit number and find the digit in the:
+#1) thousands place
+#2) hundreds place
+#3) tens place
+#4) ones place
+
+puts 1234
+print "1) " 
+puts 1234 / 1000
+print "2) "  
+puts 1234 % 1000 / 100
+print "3) " 
+puts 1234 % 1000 % 200 / 10
+print "4) " 
+puts 1234 % 1000 % 230
