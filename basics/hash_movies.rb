6d41a07ba3ffc87755f6db75eea879a3ea567f40
Clayton Bradley <cbradley2b@gmail.com>
2016-05-12 22:23:21 -0400

    use hash to store a list

---
+++ basics/hash_movies.rb
@@ -0,0 +1,9 @@
+#Write a program that uses a hash to store a list 
+#of movie titles with the year they came out. 
+#Then use the puts command to make your program print out 
+#the year of each movie to the screen. 
+
+a = {:the_crow => '1994', :matrix => '1999', :captain_america => "2011"}
+puts a[:the_crow]
+puts a[:matrix]
+puts a[:captain_america]
