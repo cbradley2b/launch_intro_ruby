5fcb8f4d89c5f54cb0ea431134a9f86d22084105
Clayton Bradley <cbradley2b@gmail.com>
2016-05-12 22:34:11 -0400

    add arrays exercise

---
+++ basics/array_movies.rb
@@ -0,0 +1,9 @@
+#Use the dates from the hash example and store them 
+#in an array. Then make your program output the same 
+#thing as the hash exercise 3.
+
+movies = [ 1994, 1999, 2011 ]
+
+puts movies[0]
+puts movies[1]
+puts movies[2]
