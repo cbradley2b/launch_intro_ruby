e5793e7606f9231f95c3dbb9e364bea1758c7715
Clayton Bradley <cbradley2b@gmail.com>
2016-05-12 22:50:29 -0400

    factorial outputs

---
+++ basics/factorial.rb
@@ -0,0 +1,12 @@
+#Write a program that outputs the factorial of the 
+#numbers 5, 6, 7, and 8
+
+fac5 =  5 * 4 * 3 * 2 * 1  
+fac6 =  6 * fac5 
+fac7 =  7 * fac6 
+fac8 =  8 * fac7 
+
+puts fac5
+puts fac6
+puts fac7
+puts fac8
