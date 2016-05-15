60a95300bd8b1eee2d9e5974883497b9a70a1982
Clayton Bradley <cbradley2b@gmail.com>
2016-05-15 09:20:20 -0400

    scope and hash updates

--- basics/hash_movies.rb
+++ basics/hash_movies.rb
@@ -3,7 +3,10 @@
 #Then use the puts command to make your program print out 
 #the year of each movie to the screen. 
 
-a = {:the_crow => '1994', :matrix => '1999', :captain_america => "2011"}
+a = { the_crow: 1994, 
+      matrix: 1999, 
+      captain_america: 2011 }
+      
 puts a[:the_crow]
 puts a[:matrix]
 puts a[:captain_america]
