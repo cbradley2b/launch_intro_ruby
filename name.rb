1ac406b2ccc459cf3c50b2c3d12e9cbcd61b2bb0
Clayton Bradley <cbradley2b@gmail.com>
2016-05-15 08:54:26 -0400

    Get the user name and greets

--- name.rb
+++ name.rb
@@ -1,3 +1,7 @@
+#Write a program called name.rb that 
+#asks the user to type in their name 
+#and then prints out a greeting message with their name included.
+
 puts "what is your name?"
 user_name = gets.chomp
 
